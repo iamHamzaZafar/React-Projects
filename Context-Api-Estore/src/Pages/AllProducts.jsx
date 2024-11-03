2a56184d3871ede644e034fa5d94563bb405faae
Hamza Zafar <link2hamzazafar@gmail.com>
2024-11-03 06:18:12 +0500

    completed pagination functionality

--- Context-Api-Estore/src/Pages/AllProducts.jsx
+++ Context-Api-Estore/src/Pages/AllProducts.jsx
@@ -1,21 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "../components/Card";
 import { useContext } from "react";
 import { DataContext } from "../context-api/DataContext";
-import Pagination from '../components/Pagination'
+import Pagination from "../components/Pagination";
 
 const AllProducts = () => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage] = useState(10);
+  const indexOfLastPost = currentPage * postsPerPage;
+
+  const indexOfFirstPost = indexOfLastPost - postsPerPage;
+
   const { data, loading, error } = useContext(DataContext);
+  console.log("data length", data.length) ;
+  const currentPosts = data.slice(indexOfFirstPost, indexOfLastPost);
+  console.log("current posts are" , currentPosts , currentPage);
+  console.log()
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error loading data</p>;
 
-
   return (
     <>
-    <Card products={data}  />
-    <Pagination />
+      {/* <Card products={data}  /> */}
+      <Card products={currentPosts} />
+      <Pagination
+        postsPerPage={postsPerPage}
+        totalPosts={data.length}
+        setCurrentPage={setCurrentPage}
+        currentPage={currentPage}
+      />
     </>
-    
   );
 };
 
--- Context-Api-Estore/src/components/Pagination.jsx
+++ Context-Api-Estore/src/components/Pagination.jsx
@@ -1,10 +1,45 @@
-import React, { useState } from 'react'
+import React from "react";
+import { useContext } from "react";
+import { DataContext } from "../context-api/DataContext";
 
-const Pagination = () => {
-  const [pages , setPages] = useState([]) ;
+const Pagination = ({
+  postsPerPage,
+  totalPosts,
+  setCurrentPage,
+  currentPage,
+}) => {
+  const pageNumbers = [];
+
+  for (let i = 1; i <= Math.ceil(totalPosts / postsPerPage); i++) {
+    pageNumbers.push(i);
+  }
+
+  const paginate = (pageNumber, e) => {
+    e.preventDefault();
+    setCurrentPage(pageNumber);
+  };
+
   return (
-    <div>Pagination</div>
-  )
-}
+    <nav className="w-4/5 mx-auto flex justify-end mb-10 mt-4">
+      <p className="text-gray-600 mr-5 text-center">Pages</p>
+      <ul className="flex space-x-2">
+        {pageNumbers.map((number) => (
+          <li
+            key={number}
+            className={`${
+              currentPage === number
+                ? "bg-blue-500 text-white"
+                : "bg-gray-200 text-gray-700 hover:bg-blue-400 hover:text-white"
+            } flex items-center justify-center w-8 h-8 rounded cursor-pointer`}
+          >
+            <a onClick={(e) => paginate(number, e)} href="#!" className="w-full h-full flex items-center justify-center">
+              {number}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
 
-export default Pagination
+export default Pagination;
